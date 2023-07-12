673577f7b3b523a422f088301da7d8dadb19a3d7
jfchicho90 <139398443+jfchicho90@users.noreply.github.com>
2023-07-12 19:44:31 -0300

    Update sistema_Pendulo_Simple_Energias.py

--- sistema_Pendulo_Simple_Energias.py
+++ sistema_Pendulo_Simple_Energias.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-theta_posicion = 5
-theta_velocidad = 0
+def pendulo_Simple(tiempo_Oscilacion, posicion_angular, velocidad_angular, t, m, L):
+    while t < tiempo_Oscilacion:
+        a = -(g/L)*(posicion_angular)
+        velocidad_angular = velocidad_angular + a*dt
+        posicion_angular = posicion_angular + velocidad_angular*dt
+        plt.plot(t, 0.5*m*velocidad_angular**2, ".b")
+        t = t + dt
+
+#variables y constantes
+posicion_angular = 5
+velocidad_angular = 0
+t = 0
+tiempo_Oscilacion = 15
 L = 5
 g = 9.81
-t = 0
 dt = 0.01
-m = 0.01
+m = 0.3
 
-def pendulo_Simple(theta_posicion, theta_velocidad, g, L, dt, t, m):
-    while t < 5:
-        a = -(g/L)*(theta_posicion)
-        theta_velocidad = theta_velocidad + a*dt
-        theta_posicion = theta_posicion + theta_velocidad*dt
-        plt.plot(t, 0.5*m*theta_velocidad**2, ".b", t, m*g*(L)*(1 - np.cos(theta_posicion)), ".r")
-        t = t + dt
-        
-pendulo_Simple(theta_posicion, theta_velocidad, g, L, dt, t, m)
+#invocacion 
+pendulo_Simple(tiempo_Oscilacion, posicion_angular, velocidad_angular, t, m, L)
 
+#grafica
 plt.show()
+
+
